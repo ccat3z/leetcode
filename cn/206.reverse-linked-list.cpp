fdd8a4b1d79bf9deab0a614f5dcd7df297899485
ccat3z <fzhang.chn@foxmail.com>
2022-03-22 22:12:57 +0800

    add cn/206

---
+++ cn/206.reverse-linked-list.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (72.76%)
+ * Likes:    2382
+ * Dislikes: 0
+ * Total Accepted:    942.6K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * 
+ * 
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return head;
+
+        ListNode *newTail = head->next;
+        ListNode *newHead = reverseList(head->next);
+        newTail->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+};
+// @lc code=end
+
+int main() {
+    ListNode n1(1), n2(2), n3(3);
+    n1.next = &n2;
+    n2.next = &n3;
+
+    ListNode *head = Solution().reverseList(&n1);
+    while (head) {
+        cout << head->val;
+        head = head->next;
+    }
+    cout << endl;
+    
+    return 0;
+}
