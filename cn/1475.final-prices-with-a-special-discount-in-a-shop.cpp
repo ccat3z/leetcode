9790dde028c401908c1432a0ae15edd359f82e62
ccat3z <fzhang.chn@foxmail.com>
2022-09-01 14:33:59 +0800

    cn/1475: rewrite with stack

--- cn/1475.final-prices-with-a-special-discount-in-a-shop.cpp
+++ cn/1475.final-prices-with-a-special-discount-in-a-shop.cpp
@@ -57,6 +57,7 @@
  * 
  */
 #include <vector>
+#include <stack>
 #include <iostream>
 #include "prettyprint.h"
 using namespace std;
@@ -65,13 +66,16 @@ using namespace std;
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        for (int i = 0; i < prices.size(); i++) {
-            for (int j = i + 1; j < prices.size(); j++) {
-                if (prices[j] <= prices[i]) {
-                    prices[i] -= prices[j];
-                    break;
-                }
+        stack<int> mins;
+        mins.emplace(0);
+
+        for (int i = prices.size() - 1; i >= 0; i--) {
+            while (!mins.empty() && mins.top() > prices[i]) {
+                mins.pop();
             }
+            prices[i] -= mins.top();
+            mins.emplace(prices[i] + mins.top());
+            // cout << mins.size() << ' ' << mins.top() << endl;
         }
 
         return prices;
