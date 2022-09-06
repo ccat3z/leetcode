682ab5be3f24a5ec0e78b132914b8b72f669ed4b
ccat3z <fzhang.chn@foxmail.com>
2022-09-06 16:55:52 +0800

    add cn/828

---
+++ cn/828.count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=828 lang=cpp
+ *
+ * [828] 统计子串中的唯一字符
+ *
+ * https://leetcode-cn.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/description/
+ *
+ * algorithms
+ * Hard (51.66%)
+ * Likes:    229
+ * Dislikes: 0
+ * Total Accepted:    16.8K
+ * Total Submissions: 26.6K
+ * Testcase Example:  '"ABC"'
+ *
+ * 我们定义了一个函数 countUniqueChars(s) 来统计字符串 s 中的唯一字符，并返回唯一字符的个数。
+ * 
+ * 例如：s = "LEETCODE" ，则其中 "L", "T","C","O","D" 都是唯一字符，因为它们只出现一次，所以
+ * countUniqueChars(s) = 5 。
+ * 
+ * 本题将会给你一个字符串 s ，我们需要返回 countUniqueChars(t) 的总和，其中 t 是 s 的子字符串。输入用例保证返回值为 32
+ * 位整数。
+ * 
+ * 注意，某些子字符串可能是重复的，但你统计时也必须算上这些重复的子字符串（也就是说，你必须统计 s 的所有子字符串中的唯一字符）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: s = "ABC"
+ * 输出: 10
+ * 解释: 所有可能的子串为："A","B","C","AB","BC" 和 "ABC"。
+ * ⁠    其中，每一个子串都由独特字符构成。
+ * ⁠    所以其长度总和为：1 + 1 + 1 + 2 + 2 + 3 = 10
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: s = "ABA"
+ * 输出: 8
+ * 解释: 除了 countUniqueChars("ABA") = 1 之外，其余与示例 1 相同。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "LEETCODE"
+ * 输出：92
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 10^5
+ * s 只包含大写英文字符
+ * 
+ * 
+ */
+// TODO: improve memory usage
+#include <vector>
+#include <string>
+#include <utility>
+#include <iostream>
+#include "prettyprint.h"
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int uniqueLetterString(string s) {
+        vector<int> idxs[26];
+
+        for (auto & idx : idxs) idx.emplace_back(-1);
+
+        for (int i = 0; i < s.size(); i++) {
+            idxs[s[i] - 'A'].emplace_back(i);
+        }
+
+        int res = 0;
+        for (auto & idx : idxs) {
+            if (idx.size() == 1) continue;
+            idx.emplace_back(s.size());
+            // cout << idx << endl;
+
+            for (int i = 0; i < idx.size() - 2; i++) {
+                res += (idx[i+1] - idx[i]) * (idx[i+2] - idx[i+1]);
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
+
+int main() {
+    vector<pair<string, int>> cases = {
+        {"ABC", 10},
+        {"ABA", 8},
+        {"LEETCODE", 92}
+    };
+    for (auto & [s, res] : cases) {
+        cout << "# " << s << endl;
+        cout << Solution().uniqueLetterString(s) << " == " << res << endl;
+    }
+    return 0;
+}
